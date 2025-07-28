241ca30374e0eb21b79cbee5767b709f2645aaf8
Sachin Yadav <139240460+Sachinyadav00@users.noreply.github.com>
2025-07-28 10:32:20 +0530

    Update Footer.jsx

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -126,7 +126,7 @@ const Footer = () => {
           Email:
         </h6>
 
-        <p className='mb-0'>amit@gmail.com </p>
+        <p className='mb-0'>sachin@gmail.com </p>
         </ListGroupItem>   
 
         <ListGroupItem   className='ps-0 border-0 d-flex align-items-center gap-3'>
